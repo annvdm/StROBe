69498cd42bcea827104987e1f5a32038b1c9903e
Bram van der Heijde <bram.vanderheijde@kuleuven.be>
2018-06-13 14:22:24 +0200

    Restructure
    Signed-off-by: Bram van der Heijde <bram.vanderheijde@kuleuven.be>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='StROBe',
+    version='',
+    packages=['StROBe'],
+    package_dir={'StROBe': 'StROBe'},
+    package_data={'': '*.txt'},
+    url='',
+    license='',
+    author='Ruben Baetens',
+    author_email='',
+    description=''
+)
