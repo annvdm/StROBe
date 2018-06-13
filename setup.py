82757f94b2cf4a4e79868fdac84dfaaccedd5ae7
Bram van der Heijde <bram.vanderheijde@kuleuven.be>
2018-06-13 15:41:38 +0200

    Add version number
    Signed-off-by: Bram van der Heijde <bram.vanderheijde@kuleuven.be>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='StROBe',
-    version='',
+    version='0.1',
     packages=['StROBe'],
     package_dir={'StROBe': 'StROBe'},
     package_data={'': '*.txt'},
