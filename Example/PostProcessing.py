a529105472e526e19772db15da564ded9b041321
anneliesvdm <annelies.vandermeulen@kuleuven.be>
2018-06-15 12:12:00 +0200

    Written methods to calculate comfort temperature averages

--- Example/PostProcessing.py
+++ Example/PostProcessing.py
@@ -1,6 +1,11 @@
+from __future__ import division
 import os
 import pandas as pd
-
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import modesto.utils as ut
+from pkg_resources import resource_filename
 
 def get_path(neighborhood, b_type=None):
     """
@@ -92,6 +97,64 @@ def simple_average(data):
     return data.mean(axis=1)
 
 
+def find_edges(df):
+    edges = df.diff()[df.diff() < -10**-4].index.values
+
+    return edges
+
+
+def min_temp_profile(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
+    df = df.copy()
+    time_step = df.index[1] - df.index[0]
+    tamb = ut.resample(tamb, time_step)
+
+    edges = find_edges(df)
+
+    cool_down_flag = False
+    for index, item in df.iteritems():
+        if index in edges:
+            cool_down_flag = True
+        if cool_down_flag:
+            timeindex = start_time + pd.Timedelta(seconds=index)
+            T_new = cool_down(df[index - time_step], tamb[timeindex], time_step, R*C)
+            if T_new >= df[index]:
+                    df[index] = T_new
+            else:
+                cool_down_flag = False
+
+    df = df.reindex(index=df.index[::-1])
+
+    warm_up_flag = False
+    edges = find_edges(df)
+    for index, item in df.iteritems():
+        if index in edges:
+            warm_up_flag = True
+        if warm_up_flag:
+            T_new = warm_up(df[index+time_step], heat, time_step, C)
+            if T_new >= df[index]:
+                    df[index] = T_new
+            else:
+                warm_up_flag = False
+
+    df = df.reindex(index=df.index[::-1])
+    return df
+
+
+def min_temp_average(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
+    for column in df:
+        df[column] = min_temp_profile(df[column], R, C, heat, tamb, start_time)
+
+    return simple_average(df)
+
+
+def cool_down(tstart, tamb, time_step, RC):
+    return (tstart - tamb) * np.exp(-time_step / RC) + tamb
+
+
+def warm_up(tstart, heat, time_step, C):
+    return tstart - heat/C*time_step
+
+
 def save_data(target_file, df):
     shape = (len(df.index), len(df.columns)+1)
     df.to_csv('tempfile.txt', header=None, sep=' ')
@@ -111,12 +174,23 @@ def main():
     neighborhoods = ['TermienWest', 'TermienEast'] # , 'Boxbergheide', 'Winterslag', 'OudWinterslag', 'ZwartbergSouth',
                      # 'ZwartbergNWest', 'ZwartbergNWest', 'ZwartbergNEast', 'WaterscheiGarden'
 
+    datapath = resource_filename('modesto', 'Data')
+    tamb = ut.read_time_data(datapath, name='Weather/weatherData.csv', expand=True)['Te']
+
     for neigh in neighborhoods:
-        for name in simple_avg_files:
+        print '\n', neigh
+        print '-------------------'
+        for name in simple_avg_files + min_temp_files:
+            print '\n', name + '.txt'
             df = pd.DataFrame(columns=building_types)
-            for type in building_types:
-                all_data = read_time_data(get_path(neigh, type), name + '.txt')
-                df[type] = simple_average(all_data)
+            for b_type in building_types:
+                print b_type
+                all_data = read_time_data(get_path(neigh, b_type), name + '.txt')
+                if name in simple_avg_files:
+                    df[b_type] = simple_average(all_data)
+                else:
+                    # TODO Add good building model and parameters!
+                    df[b_type] = min_temp_average(all_data, 0.5, 1000000, 10, tamb - 273.15)
 
             save_data(os.path.join(get_path(neigh), name + '.txt'), df)
 
