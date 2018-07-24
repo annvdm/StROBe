d9d17f052bb8b0aeb69623f4fd6ae5b6cc424969
anneliesvdm <annelies.vandermeulen@kuleuven.be>
2018-07-24 11:17:56 +0200

    Added documentation to PostProcessing file

--- Example/PostProcessing.py
+++ Example/PostProcessing.py
@@ -94,38 +94,73 @@ def expand_df(df, start_year=2014):
 
 
 def simple_average(data):
+    """
+    Take an average over all the columns of a dataframe
+
+    :param data: A pandas.DataFrame object
+    :return: A pandas.Series object containing the vaerage
+    """
     return data.mean(axis=1)
 
 
 def find_edges(df):
+    """
+    Finds the locations in a pandas.Series object where the value decreases
+
+    :param df: pandas.Series object
+    :return: List of index values where the corresponding value is about to decrease
+    """
     edges = df.diff()[df.diff() < -10**-4].index.values
 
     return edges
 
 
 def min_temp_profile(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
-    df = df.copy()
-    time_step = df.index[1] - df.index[0]
-    tamb = ut.resample(tamb, time_step)
+    """
+    Calculates the actual minimum possible temperature profile for a building
 
+    :param df: Minimum comfort temperature profile
+    :param R: Resistance of the building [K/W]
+    :param C: Capacitance of the building [J/kg/K]
+    :param heat: Nominal heat that can be delivered to the building
+    :param tamb: Ambient temperature
+    :param start_time: Time from which the actual minimum temperature should be calculated
+    :return:
+    """
+
+    df = df.copy()
+    time_step = df.index[1] - df.index[0]  # Time step of the minimum temperature profile
+    tamb = ut.resample(tamb, time_step)  # Resample ambien temperature to min. temp. profile's sampling time
+
+    # Find the places where the comfort temperature decreases
+    # From these points on the cooling down of the temperature on the building has to be calculated
     edges = find_edges(df)
 
-    cool_down_flag = False
+    cool_down_flag = False  # Flag to indicate whether the building has cooled down to the minimum allowed temperature
+
+    # Looping over all minimum temperatures
     for index, item in df.iteritems():
         if index in edges:
+            # Building starts to cool down
             cool_down_flag = True
         if cool_down_flag:
+            # Calculate new temperature after another time step of cooling down
             timeindex = start_time + pd.Timedelta(seconds=index)
             T_new = cool_down(df[index - time_step], tamb[timeindex], time_step, R*C)
             if T_new >= df[index]:
+                # Temperature in the building has not cooled down yet to minimum allowed value
                     df[index] = T_new
             else:
+                # Temperature in the building has cooled down to minimum allowed value
                 cool_down_flag = False
 
+    # Fing the places where the minimum comfort temperature increases
+    # The dataframe is now reversed in direction, so a warm-up in the original dataframe is now a cool down again!
+    # So code is the same as before!
     df = df.reindex(index=df.index[::-1])
+    edges = find_edges(df)
 
     warm_up_flag = False
-    edges = find_edges(df)
     for index, item in df.iteritems():
         if index in edges:
             warm_up_flag = True
@@ -141,6 +176,21 @@ def min_temp_profile(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
 
 
 def min_temp_average(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
+    """
+    Average an minimum temperature profiles to a single profile
+
+    Note: At the moment only a single type of building is possible (with only one R, C and nominal heat value possible)
+
+    :param df: The dataframe containing all profiles (rows=time, columns=profiles)
+    :param R: Average resistance of all buildings
+    :param C: Average capacitance of all buildings
+    :param heat: Average nominal heat that can be added to the buildings
+    :param tamb: Ambient temperature profile
+    :param start_time: Start time of the average minimum temperature profile
+    :return:
+    """
+
+    # Calculate the minimum temperature profile for each building
     for column in df:
         df[column] = min_temp_profile(df[column], R, C, heat, tamb, start_time)
 
@@ -148,14 +198,41 @@ def min_temp_average(df, R, C, heat, tamb, start_time=pd.Timestamp('20140101')):
 
 
 def cool_down(tstart, tamb, time_step, RC):
+    """
+    Method that calculates cool down of a building
+
+
+    :param tstart: Start temperature of the building
+    :param tamb: Ambient temperature during the considered time period
+    :param time_step: Length of the time step during which the building is warmed up
+    :param RC: RC value of the building
+    :return: Temperature on the building at the end of the time ste[
+    """
     return (tstart - tamb) * np.exp(-time_step / RC) + tamb
 
 
 def warm_up(tstart, heat, time_step, C):
+    """
+    Method that calculates warm up of a building (in reverse direction! So starting from the warm temperature and
+    calculating what the tenperature was in the time step before!)
+
+    :param tstart: Start temperature of the building
+    :param heat: Heat that is added to the building during the time step [W]
+    :param time_step: Length of the time step during which the building is warmed up
+    :param C: Capacity of the building
+    :return:
+    """
     return tstart - heat/C*time_step
 
 
 def save_data(target_file, df):
+    """
+    Save the the dta to a Modelica-compatible format
+
+    :param target_file:
+    :param df:
+    :return:
+    """
     shape = (len(df.index), len(df.columns)+1)
     df.to_csv('tempfile.txt', header=None, sep=' ')
     header = '#1\ndouble data' + str(shape) + '\n'
@@ -168,18 +245,26 @@ def save_data(target_file, df):
 
 
 def main():
+
+    # Files of which a simple average can be taken
     simple_avg_files = ['mDHW', 'P', 'Q', 'QCon', 'QRad']
+
+    # Files for zhich the min-temperature average algorithm should be used
     min_temp_files = ['sh_bath', 'sh_day', 'sh_night']
+
+    # Information necessary to fond the StROBe files:
     building_types = ['D', 'SD', 'T']
     neighborhoods = ['TermienWest', 'TermienEast'] # , 'Boxbergheide', 'Winterslag', 'OudWinterslag', 'ZwartbergSouth',
                      # 'ZwartbergNWest', 'ZwartbergNWest', 'ZwartbergNEast', 'WaterscheiGarden'
 
+    # Information on weather data, needed to calculate minimum temperature averages
     datapath = resource_filename('modesto', 'Data')
     tamb = ut.read_time_data(datapath, name='Weather/weatherData.csv', expand=True)['Te']
 
     for neigh in neighborhoods:
         print '\n', neigh
         print '-------------------'
+        
         for name in simple_avg_files + min_temp_files:
             print '\n', name + '.txt'
             df = pd.DataFrame(columns=building_types)
